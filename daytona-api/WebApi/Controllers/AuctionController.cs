c025803707abf30f002576b836f384942ff75bab
aopalko <aopalko@four-ages.com>
2025-05-30 12:24:02 +0300

    added mock data

---
+++ daytona-api/WebApi/Controllers/AuctionController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuctionController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuctionController(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAuctions()
+    {
+        using StreamReader reader = new StreamReader("__MockResponse__/main-vehicle.json");
+        var json = await reader.ReadToEndAsync();
+        
+        // dynamic result = JsonConvert.DeserializeObject(json);
+        
+        return Content(json, "application/json");
+    }
+    
+}
